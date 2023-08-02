52ad0960df6f9abac10e199b1119402a4409fd6d
Jay Lee <horangii@Jays-MacBook-Pro.local>
2023-08-02 23:30:51 +0900

    dispatcherServlet desc

--- src/main/java/springprojects/helloboot/HelloController.java
+++ src/main/java/springprojects/helloboot/HelloController.java
@@ -1,7 +1,12 @@
 package springprojects.helloboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+@RequestMapping("/hello")
 public class HelloController {
     private final HelloService helloService;
 
@@ -9,6 +14,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping
+    @ResponseBody // responseBody를 사용하지 않으면 view라고 불리는 템플릿을 찾기 때문에 찾을 수 없다. (404)
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name)); // null인지 체크함 null이라면 예외를 던지고, null이 아니라면 값을 그대로 리턴
     }
--- src/main/java/springprojects/helloboot/HellobootApplication.java
+++ src/main/java/springprojects/helloboot/HellobootApplication.java
@@ -3,42 +3,26 @@ package springprojects.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		// dispatcherServlet을 사용할 때는 GenericApplicationContext가 아닌 GenericWebApplicationContext를 사용한다.
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh();
 
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("frontcontroller", new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-						String name = req.getParameter("name");
-
-						HelloController helloController = applicationContext.getBean(HelloController.class);
-						String ret = helloController.hello(name);
-
-						resp.setStatus(HttpStatus.OK.value());
-						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().println(ret);
-					}
-				}
-			}).addMapping("/*");
+			// dispatcherServlet은 WebApplicationContext 타입을 사용해야 한다.
+			// dispatcherServlet에 applicationContext를 넘겨서 매핑을 하다가 작업을 위임할(요청을 Dispatch할) 오브젝트를 찾아야 하는데
+			// 그 때 사용할 서블릿 컨테이너를 전달해줌.
+			servletContext.addServlet("dispatcherServlet",
+					new DispatcherServlet(applicationContext))
+					.addMapping("/*");
 		});
 		webServer.start();
 	}
